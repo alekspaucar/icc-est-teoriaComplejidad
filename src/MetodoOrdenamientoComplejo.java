c8da97c1fb07a9947fdcfd7405a7cb236e64451d
alexpau <locutudgamer00@gmail.com>
2024-11-14 18:29:28 -0500

    ya loco ya esta

--- src/MetodoOrdenamientoComplejo.java
+++ src/MetodoOrdenamientoComplejo.java
@@ -4,7 +4,7 @@ public class MetodoOrdenamientoComplejo {
         int n = arr.length;
         //metodo burbuja con ajuste
         for (int i = 0 ; i < n - 1; i ++){
-            for (int j = 0 ; j< n - i - 1 ; i++){
+            for (int j = 0 ; j< n - i - 1 ; j++){
                 if (arr[j] > arr[ j + 1 ]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
